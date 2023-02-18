e58f00e7e2fafbf73b4c9a545248d73731a1e10f
ghua-ac <ghua@ieee.org>
2023-02-18 22:23:56 +0800

    hide_files_for_optimization

--- DQA/run.sh
+++
@@ -1,6 +0,0 @@
-python main.py --Dratio 1
-python main.py --Dratio 0.5
-python main.py --Dratio 0.25
-
-
-
--- IDA/run.sh
+++
@@ -1,3 +0,0 @@
-python main.py --dataset 'Cifar10'  --num_classes 10
-python main.py --dataset 'Cifar100'  --num_classes 100
-python main.py --dataset 'FOOD101' --num_classes 101
